e7b01b3acceab6c7ea438a4cd5dbd53c25ad8dd3
ZenAzius <lalurizaldyprawira@gmail.com>
2025-06-02 12:56:42 +0700

    p

---
+++ src/sewa_kost/Sewa_Kost.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sewa_kost;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Sewa_Kost {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       new Sewa_Kamar().setVisible(true);
+    }
+    
+}
